06c66c74390a416663578fad1ec94ef2de6c1771
Shashank Bandari <shashank2bandari@gmail.com>
2025-03-09 03:35:10 +0530

    exempted notch area full screen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import WebView from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar hidden />
-      <WebView source={{ uri: 'https://www.x.com' }} style={{ flex: 1 }} />
-    </View>
+      <View style={{ flex: 1 }}>
+        <WebView source={{ uri: 'https://www.x.com' }} style={{ flex: 1 }} />
+      </View>
+    </SafeAreaView>
   );
 }
