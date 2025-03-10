6986177b8c83b8a257c7eccf1ac11a63c63ac179
Shashank Bandari <shashank2bandari@gmail.com>
2025-03-10 13:13:05 +0530

    apk any app done

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import WebView from 'react-native-webview';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar hidden />
-      <View style={{ flex: 1 }}>
-        <WebView source={{ uri: 'https://www.x.com' }} style={{ flex: 1 }} />
-      </View>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea} edges={['top', 'left', 'right']}>
+        <StatusBar  />
+        <View style={styles.container}>
+          <WebView source={{ uri: 'https://sbgoud.github.io/redirectapp/' }} />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff', // Ensure background is set to avoid a blank screen
+  },
+  container: {
+    flex: 1,
+  },
+});
